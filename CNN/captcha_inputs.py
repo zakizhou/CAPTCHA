f14b9268e1cca22221fbba99ad1672577cc0f67f
zakizhou <windows98@ruc.edu.cn>
2016-10-15 16:50:51 +0800

    change variable to get_variable

---
+++ CNN/captcha_inputs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+"""
+author : windows98@ruc.edu.cn
+"""
+import tensorflow as tf
+import os
+import numpy as np
+import re
+
+def read_and_decode(path):
+    os.chdir(path)
+    filenames = os.listdir(os.getcwd())
+    labels = np.array(map(lambda x:decode_label(re.findall("_(.*?)\.",x)[0]),filenames))
+    filename_tensor = tf.convert_to_tensor(filenames)
+    label_tensor = tf.convert_to_tensor(labels)
+    input_queue = tf.train.slice_input_producer([filename_tensor,label_tensor])
+    image_name,label = input_queue
+    image_file = tf.read_file(image_name)
+    image = tf.image.decode_png(image_file)
+    image.set_shape([60,160,3])
+    return image,label
+
+def decode_label(label):
+    one_hot_label = np.zeros([4,10])
+    index = [[0,1,2,3],map(int,list(label))]
+    one_hot_label[index] = 1.0
+    return one_hot_label
+
+def inputs(path,batch_size,min_after_dequeue,capacity):
+    image,label = read_and_decode(path)
+    image_batch,label_batch  = tf.train.shuffle_batch([image,label],
+                                                      batch_size=batch_size,
+                                                      min_after_dequeue=min_after_dequeue,
+                                                      capacity=capacity)
+    return image_batch,label_batch
---
+++ CNN/captcha_train.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import tensorflow as tf
+import captcha
+import captcha_inputs
+
+def train():
+    images,labels = captcha_inputs.inputs("/home/zhoujie/TensorFlow/application/CAPTCHA/images/PNG",5,100,300)
+    logits = captcha.inference(images)
+    loss = loss(logits,labels)
+    train_op = captcha.train(loss)
+    with tf.Session() as sess:
+        init = tf.initialize_all_variables()
+        sess.run(init)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess,coord=coord)
+        try:
+            sess.run([train_op,loss])
+        except Exception:
+            pass
+
+if __name__ == "__main__":
+   train()
