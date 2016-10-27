49e20680ca1af883275506b9c8892e684eb03bd4
zakizhou <windows98@ruc.edu.cn>
2016-10-27 21:40:34 +0800

    change the file of generate images and update README

---
+++ CNN/captcha_generator.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 21:10:47 2016
+
+@author: Windo
+"""
+
+from captcha.image import ImageCaptcha
+import random
+import string
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p",
+                        "--path",
+                        required=False,
+                        help="path to store generated images")
+    parser.add_argument("-n",
+                        "--number",
+                        required=True,
+                        help="number of images generated")
+    #parser.add_argment("-")
+    maker = ImageCaptcha()
+    args = vars(parser.parse_args())
+    if("path" not in args):
+        path = ""
+    else:
+        print("2")
+        path = args['path']
+    print(args)
+    for i in range(int(args['number'])):
+        number_to_write = "".join([random.choice(string.digits) for j in range(4)])
+        maker.write(number_to_write,path+str(i)+"_"+number_to_write+".PNG")
+        
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CAPTCHA
+Before we start training, we need to make some data for training.
+To generate 50000 images in the path ~/images,just do:
+```python
+python CNN/captcha_generator.py -n 50000 -p "~/images/"
+```
+
